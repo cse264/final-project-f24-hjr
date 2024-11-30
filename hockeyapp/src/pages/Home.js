8f1cd2c24de7fdd801558c1a92cbd6524784efda
Hannah Kaufman <hfk225@lehigh.edu>
2024-11-30 17:59:49 -0500

    gif added

--- hockeyapp/src/pages/Home.js
+++ hockeyapp/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo2 from '/Users/hannahkaufman/Documents/CSE 264/Final Project/final-project-f24-hjr/hockeyapp/src/logo-alt.gif'
 
 function Home() {
   return (
     <div>
-      <h1>Welcome to HockeyBuilder</h1>
-      <p>Build your dream hockey team and compete against others!</p>
+        <h1>Welcome to HockeyBuilder</h1>
+        <p>Build your dream hockey team and compete against others!</p>
+        <img src={logo2} alt="HockeyBuilder Logo" className="logo2" />
     </div>
   );
 }
